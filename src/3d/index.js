29e7325f167944b846c4cfc31e42900080867313
Remi Tran <tranremi@live.fr>
2019-08-12 17:32:44 +0200

    ScreenPos > fix

--- src/3d/index.js
+++ src/3d/index.js
@@ -1,8 +1,8 @@
-export function getScreenPos(mesh, sizes, vector3) {
+export function getScreenPos(mesh, sizes, camera, vector3) {
 	var widthHalf = 0.5 * sizes.viewport.width;
 	var heightHalf = 0.5 * sizes.viewport.height;
 	vector3.setFromMatrixPosition(mesh.matrixWorld);
-	vector3.project(this.camera);
+	vector3.project(camera);
 	vector3.x = Math.round(((vector3.x * widthHalf) + widthHalf) * 100) / 100;
 	vector3.y = Math.round((-(vector3.y * heightHalf) + heightHalf) * 100) / 100;
 	return {
