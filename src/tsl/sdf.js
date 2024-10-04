1fd43532ae2d73aae78ec5bae1705f6b8cb214fe
trinketmage <tranremi@live.fr>
2024-10-04 17:56:21 +0200

    TSL.sdBox > fix math

--- src/tsl/sdf.js
+++ src/tsl/sdf.js
@@ -8,5 +8,5 @@ import {
 
 export var sdBox = Fn(([st, b]) => {
     const d = abs(st).sub(b);
-    return length(max(d, 0)).add(min(max(st.x, st.y), 0));
+    return length(max(d, 0)).add(min(max(d.x, d.y), 0));
 });
