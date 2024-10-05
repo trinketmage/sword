e36ea466c6a5b8753c1ca017916bc587db8942d3
trinketmage <tranremi@live.fr>
2024-10-05 10:54:18 +0200

    sdf > add circle

--- src/tsl/sdf.js
+++ src/tsl/sdf.js
@@ -10,3 +10,7 @@ export var sdBox = Fn(([st, b]) => {
     const d = abs(st).sub(b);
     return length(max(d, 0)).add(min(max(d.x, d.y), 0));
 });
+
+export var sdCircle = Fn(([st, radius]) => {
+    return length(st).sub(radius);
+});
