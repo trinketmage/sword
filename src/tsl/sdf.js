65b9cd9cc681709bd7a4be0d833c11f5fb1cbda2
trinketmage <tranremi@live.fr>
2024-10-05 16:27:18 +0200

    tsl > sdRoundedBox

--- src/tsl/sdf.js
+++ src/tsl/sdf.js
@@ -4,11 +4,20 @@ import {
     length,
     min,
     max,
+    select,
 } from 'three/tsl';
 
 export var sdBox = Fn(([st, b]) => {
     const d = abs(st).sub(b);
     return length(max(d, 0)).add(min(max(d.x, d.y), 0));
+});
+
+export const sdRoundedBox = Fn(([st, b, r]) => {
+    r = r.toVar();
+    r.xy = select(st.x.greaterThan(0.0), r.xy, r.zw);
+    r.y = select(st.y.greaterThan(0.0), r.x, r.y);
+    const q = abs(st).sub(b).add(r.x);
+    return min(max(q.x,q.y),0.0).add(length(max(q,0.0))).sub(r.x);
 });
 
 export var sdCircle = Fn(([st, radius]) => {
