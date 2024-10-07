1adca8cc563b40fc9b7b9f0299e24da5b97ee043
trinketmage <tranremi@live.fr>
2024-10-08 00:47:37 +0200

    tsl > sdf glyph

--- src/tsl/sdf.js
+++ src/tsl/sdf.js
@@ -31,9 +31,7 @@ export var sdCircle = Fn(([st, radius]) => {
 });
 
 export var sdGlyph = Fn((map, uv) => {
-    var alpha = 1.0;
     var msd = texture(map, uv);
-    var sd = median(msd.r, msd.g, msd.b) - 0.5;
-    alpha *= clamp(sd / fwidth(sd) + 0.5, 0.0, 1.0);
-    return alpha;
+    var sd = median(msd.r, msd.g, msd.b).sub(0.5);
+    return clamp(sd.div(fwidth(sd).add(0.5)), 0.0, 1.0);
 });
