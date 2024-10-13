15edbc82299365952d2a61cbdb6e54ca8ec00217
trinketmage <tranremi@live.fr>
2024-10-13 11:06:28 +0200

    📝(tsl-rotate2d): reference update

--- src/tsl/matrices.js
+++ src/tsl/matrices.js
@@ -5,6 +5,7 @@ import {
     sin,
 } from 'three/tsl';
 
+// be mindful of https://github.com/mrdoob/three.js/wiki/Three.js-Shading-Language#uv-utils
 export var rotate2d = Fn(([ angle ]) => {
     return mat2(
         cos(angle), sin(angle).mul(-1),
