a183f51eb08916fb6f56e76bfad0d33412988933
trinketmage <tranremi@live.fr>
2024-11-25 19:26:26 +0100

    feat(math): random

--- src/tsl/math.js
+++ src/tsl/math.js
@@ -2,8 +2,16 @@ import {
     Fn,
     min,
     max,
+    fract,
+    sin,
+    dot,
+    vec2
 } from 'three/tsl';
 
 export var median = Fn(([r, g, b]) => {
     return max(min(r, g), min(max(r, g), b));
+});
+
+export var random = Fn(([co]) => {
+    return fract(sin(dot(co ,vec2(12.9898,78.233))).mul(43758.5453));
 });
